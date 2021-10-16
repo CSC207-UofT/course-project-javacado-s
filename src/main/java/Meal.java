363ebfb73ad26c31360e160e3d0931ea204d7189
songzh17 <zhengdong.song@mail.utoronto.ca>
2021-10-16 12:02:47 -0400

    UPDATED meal class (Version @12:01 PM) NEW: modified meal class (change the way calculate the number employee & change integer to float) as discussed before.

--- src/main/java/Meal.java
+++ src/main/java/Meal.java
@@ -8,9 +8,9 @@ import java.util.Hashtable;
 
 public class Meal {
     private String[] mealType = {"breakfast","lunch","dinner"};
-    private Hashtable<String, Integer> priceList = new Hashtable<String, Integer>();
+    private Hashtable<String, Float> priceList = new Hashtable<String, Float>();
     private Hashtable<String, String[]> menuList = new Hashtable<String, String[]>();
-    private final Hashtable<String, Integer> NUM_EMPLOYEE = new Hashtable<String, Integer>();
+    private final Hashtable<String, Float> NUM_EMPLOYEE = new Hashtable<String, Float>();
     private String selectedMeal = "";
 
     /**
@@ -28,9 +28,9 @@ public class Meal {
 
     // a private method which sets the Meal Price List per attendee.
     private void setMealPriceList(){
-        this.priceList.put(mealType[0],7);
-        this.priceList.put(mealType[1],14);
-        this.priceList.put(mealType[2],22);
+        this.priceList.put(mealType[0],(float) 7.0);
+        this.priceList.put(mealType[1],(float) 14.0);
+        this.priceList.put(mealType[2],(float) 22.0);
     }
 
     // a private method which sets the General Menu
@@ -42,22 +42,22 @@ public class Meal {
 
     // a private method which sets the number Employee per numAttendeeGroup attendees.
     private void setEmployee(){
-        this.NUM_EMPLOYEE.put(mealType[0],1);
-        this.NUM_EMPLOYEE.put(mealType[1],2);
-        this.NUM_EMPLOYEE.put(mealType[2],3);
+        this.NUM_EMPLOYEE.put(mealType[0], (float) 0.2);
+        this.NUM_EMPLOYEE.put(mealType[1], (float) 0.4);
+        this.NUM_EMPLOYEE.put(mealType[2], (float) 0.6);
     }
 
     /**
      * @return an integer represents the price of this meal per attendee.
      */
-    public int getMealPrice(){
+    public float getMealPrice(){
         return priceList.get(this.selectedMeal.toLowerCase());
     }
 
     /**
      * @return an integer represents the number Employee per numAttendeeGroup attendees.
      */
-    public int getNumEmployee(){
+    public float getNumEmployee(){
         return NUM_EMPLOYEE.get(this.selectedMeal.toLowerCase());
     }
 
