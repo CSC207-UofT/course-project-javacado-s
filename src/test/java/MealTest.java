70d1658b5f2511b785dc907397c2046daa1ce290
songzh17 <zhengdong.song@mail.utoronto.ca>
2021-10-16 20:43:27 -0400

    TestMeal class to test the Meal class and its two public methods.

---
+++ src/test/java/MealTest.java
@@ -0,0 +1,19 @@
+import org.junit.*;
+
+import static org.junit.Assert.*;
+
+public class MealTest {
+    Meal e_meal = new Meal("breakfast");
+
+    @Test(timeout = 50)
+    public void TestgetMealPrice(){
+        assertEquals(7.0, e_meal.getMealPrice(),3);
+    }
+
+    @Test(timeout = 50)
+    public void TestgetNumEmployee(){
+        assertEquals(0.2, e_meal.getNumEmployee(),3);
+    }
+
+
+}
