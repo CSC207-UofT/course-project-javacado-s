babde74ca1e5fcec7881f80083726899b3012b85
tehzi <zxt199@gmail.com>
2021-11-02 00:29:17 -0400

    Tentative update, unchecked(!!!!!), subject to change.
    Created UserManager, User (temporary), modified Event to be Serializable.
    
    TODO:
    Modify EventManager implementation (needs to deserialize input on startup, and needs a serialize events method)
    Write tests (!!!!! I'm not 100% sure my code works, which is why I commented so much on it to make it easier to check).
    Commands
    
    Discussion:
    1. I'm not in favor of adding "past events" to our current implementation. I don't believe we've even finished implementing date/time to our existing classes yet.
    2. Is it be possible for System to hold an "active user"? Else I could also make an instance variable for UserManager to hold the current active User, but it feels strange that way. Unsure.
    
    IF WORKING ON USER IMPLEMENTATION, PLEASE PUSH TO THIS BRANCH; THIS IS A MAJOR EXTENSION AND OUR EXISTING TEST CASES OR SYSTEM MAY BREAK IF WE PUSH THIS TO MAIN BEFORE MAKING SURE IT WORKS
    I would rather not rollback changes later on.

--- src/main/java/UserManager.java
+++ src/main/java/UserManager.java
@@ -1,8 +1,16 @@
 import java.io.*;
 import java.util.ArrayList;
-import java.util.Scanner;
 
+/**
+ * This class represents the UserManager branch of our system. The UserManager does not store any instance variables
+ * (subject to change), and reads and writes client information to files inside the users folder.
+ */
 public class UserManager {
+    /*
+     * USER_DIRECTORY is the path for the list of user files stored in the system. For now, I have them saved under a
+     * folder called "users".
+     * users is a list of said files as File objects.
+     */
     final File USER_DIRECTORY = new File("users");
     private File[] users = USER_DIRECTORY.listFiles();
 
@@ -25,19 +33,29 @@ public class UserManager {
     public void createUser(String username, String password) throws Exception {
         for(File f: users){
             if(f.getName().equals(username+".txt")){
+                /*
+                 I may invest time into writing new Exception classes later, for now I'll throw generic Exceptions
+                 with descriptions.
+                 */
                 throw new Exception("Username already exists. Please choose another username.");
             }
         }
         FileWriter new_user = new FileWriter("users/" + username + ".txt");
+        /*
+        A new user file will contain only one line, the password, as they have no Events.
+         */
         new_user.write(password);
         new_user.close();
-        this.users = USER_DIRECTORY.listFiles();  /*the list of users has to be updated again after creating a user;
-                                                   login is separate from account creation*/
+        this.users = USER_DIRECTORY.listFiles();
+        /*
+         * the list of users has to be updated again after creating a new account; login is separate from account
+         * creation.
+         */
     }
 
     /**
      * Effectively the "login" method for the client.
-     * Throws an exception if:
+     * Throws an exception if (more to be added):
      *      no User with such username exists
      *      username-password mismatch
      *
@@ -50,8 +68,21 @@ public class UserManager {
     public User getUser(String username, String password) throws Exception{
         ArrayList<String> user_events = new ArrayList<>();
         for(File f: users){
+            /*
+            May replace with a try-catch. Probably should, in fact.
+             */
             if(f.getName().equals(username+".txt")){
-                BufferedReader br = new BufferedReader(new FileReader(username+".txt"));
+                /*
+                BufferedReader reads from a character-input stream, whereas FileReader reads chars at a time
+                so BufferedReader is both more efficient and easier to work with.
+                 */
+                FileReader fr = new FileReader(username+".txt");
+                BufferedReader br = new BufferedReader(fr);
+                /*
+                BufferedReader moves onto the next line in the file after each call of readLine(), I trim to make
+                sure newline characters don't mess up the equality. This may be an issue later on depending on
+                whether we allow spaces at the beginning and end of passwords.
+                 */
                 if(br.readLine().trim().equals(password)){
                     String curr;
                     while((curr=br.readLine())!=null){
@@ -61,6 +92,11 @@ public class UserManager {
                 else{
                     throw new Exception("Password does not match.");
                 }
+                br.close();
+                fr.close();
+            }
+            else{
+                throw new Exception("No User found.");
             }
         }
         return new User(username, password, user_events);
