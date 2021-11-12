e81830c89b027d365dcb189683c4867585f3d3a5
Maggie39 <yanrong.yang@mail.utoronto.ca>
2021-11-12 16:57:34 -0500

    Added some test cases in EmployeeTest

--- src/test/java/EmployeeTest.java
+++ src/test/java/EmployeeTest.java
@@ -1,6 +1,12 @@
 import employees.Employee;
 import org.junit.*;
 import static org.junit.Assert.*;
+import java.util.ArrayList;
+import java.util.Calendar;
+
+import static org.junit.Assert.*;
+import java.util.Date;
+import java.util.ArrayList;
 
 
 public class EmployeeTest {
@@ -11,16 +17,40 @@ public class EmployeeTest {
         e = new Employee("Carly", 1234);
     }
     @Test(timeout = 50)
-    public void TestgetName() {
+    public void TestGetName() {
         assertEquals("Carly", e.getName());
     }
+
     @Test(timeout = 50)
-    public void Testgetid() {
+    public void TestGetId() {
         assertEquals(1234, e.getid());
     }
+
     @Test(timeout = 50)
-    public void TestgetWage(){
+    public void TestGetWage(){
         assertEquals(10, e.getWage());
     }
+
+    @Test(timeout = 50)
+    public void TestSetAvailability(){
+        ArrayList<Date> Unavailable = new ArrayList<Date>();
+        Unavailable.add(new Date(2021, Calendar.NOVEMBER, 20, 18, 30, 24));
+        assertEquals(e.getUnavailableDates(), Unavailable);}
+
+    @Test(timeout = 50)
+    public void TestisAvailable(){assertTrue(e.isAvailable(new Date(2021,
+            Calendar.NOVEMBER, 8, 18, 30, 24)));}
+
+//  Todo: pass the TestEquals() method
+
+//    @Test(timeout = 50)
+//    public void TestEquals(){
+//        Employee e1 = new Employee("Carly", 1234);
+//        e1.setUnavailability(new Date(2021, Calendar.NOVEMBER, 20, 18, 30, 24));
+//        Employee e2 = new Employee("Carly", 1234);
+//        e2.setUnavailability(new Date(2021, Calendar.NOVEMBER, 20, 18, 30, 24));
+//        assertEquals(e1, e2);
+//    }
+
 
 }
