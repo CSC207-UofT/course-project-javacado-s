c107d6c6957f4f88f0f5594aba9b85b114b6eabd
Maggie39 <yanrong.yang@mail.utoronto.ca>
2021-11-12 14:11:00 -0500

    Added some tests to EmployeeTest for higher coverage.

--- src/test/java/EmployeeTest.java
+++ src/test/java/EmployeeTest.java
@@ -1,6 +1,11 @@
 import org.junit.*;
-import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+
+import static org.junit.Assert.*;
+import java.util.Date;
+import java.util.ArrayList;
 
 public class EmployeeTest {
     Employee e;
@@ -8,18 +13,31 @@ public class EmployeeTest {
     @Before
     public void setUp() throws Exception {
         e = new Employee("Carly", 1234);
+        e.setUnavailability(new Date(2021, Calendar.NOVEMBER, 20, 18, 30, 24));
     }
     @Test(timeout = 50)
-    public void TestgetName() {
+    public void TestGetName() {
         assertEquals("Carly", e.getName());
     }
+
     @Test(timeout = 50)
-    public void Testgetid() {
+    public void TestGetId() {
         assertEquals(1234, e.getid());
     }
+
     @Test(timeout = 50)
-    public void TestgetWage(){
+    public void TestGetWage(){
         assertEquals(10, e.getWage());
     }
+
+    @Test(timeout = 50)
+    public void TestSetAvailability(){
+        ArrayList<Date> Unavailable = new ArrayList<Date>();
+        Unavailable.add(new Date(2021, Calendar.NOVEMBER, 20, 18, 30, 24));
+        assertEquals(e.getUnavailableDates(), Unavailable);}
+
+    @Test(timeout = 50)
+    public void TestisAvailable(){assertTrue(e.isAvailable(new Date(2021,
+                Calendar.NOVEMBER, 8, 18, 30, 24)));}
 
 }
