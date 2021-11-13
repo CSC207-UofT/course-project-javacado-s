df257efc6895a3b76f65893245379345515081b7
h4kureii <59467323+h4kureii@users.noreply.github.com>
2021-11-13 13:10:37 -0500

    fixed meal setter

--- src/main/java/managers/EventManager.java
+++ src/main/java/managers/EventManager.java
@@ -54,7 +54,8 @@ public class EventManager {
      */
     public int createEvent(String name, Date date, String location,
                           int numAttendees, String selectedMeal){
-        Meal newMeal = new Meal(selectedMeal);
+        MealSetter setMeal = new MealSetter(selectedMeal);
+        Meal newMeal = setMeal.getMeal();        
         Event newEvent = new Event(this.newId, name, date, location, numAttendees, newMeal);
         this.eventList.add(newEvent);
         this.idEventMap.put(this.newId, newEvent);
