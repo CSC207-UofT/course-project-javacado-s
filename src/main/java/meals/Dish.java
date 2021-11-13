536fc76566b51a8454c31b7f7c5c1ab32206b1d6
ZS106 <90329405+ZS106@users.noreply.github.com>
2021-11-12 23:57:32 -0500

    Update Dish.java

--- src/main/java/meals/Dish.java
+++ src/main/java/meals/Dish.java
@@ -2,14 +2,26 @@ package meals;
 
 public class Dish implements Ingredient {
         private float price;
-        private String name;
-        public Dish(String name, float price){
-                this.name = name;
+        private String Ig_name;
+        private String meal_name;
+
+        public Dish(String meal_name, String Ig_name, float price){
+                this.meal_name = meal_name;
+                this.Ig_name = Ig_name;
                 this.price = price;
         }
 
         @Override
         public float getMealPrice() {
                 return this.price;
+        }
+
+        @Override
+        public String getMealName(){
+                return this.meal_name;
+        }
+
+        public String getIgName(){
+                return this.Ig_name;
         }
 }
